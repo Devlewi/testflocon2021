cfb28e7f5ec4044e756d8cdcf840267db93bd29f
Léwi jean-marc Essoh <lewimarcjean3@gmail.com>
2021-02-17 11:18:37 +0000

    Initial commit

---
+++ Javascript neige_files/phpmyvisites.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title>Vic.drago.free.fr - Erreur 404</title>
+<meta charset="utf-8" />
+<meta name="description" content="Pages persos : page d'erreur 404 en cas de page non trouv&eacute;e." />
+<meta name="keywords" content="erreur, 404, erreur 404" />
+<link rel="stylesheet" type="text/css" href="styles/base.css" />
+
+</head>
+<body>
+<h1>Erreur 404</h1>
+<p>La page que vous recherchez n'existe pas ou plus.</p>
+<p><a href="neige.php">Script simulant une chute de neige &agrave; l'&eacute;cran</a></p>
+<p>Vous pouvez &eacute;galement visiter le site suivant :</p>
+<p><a href="http://suresnes.croix-rouge.fr">La Croix-Rouge de Suresnes</a> (&eacute;galement sur <a href="http://twitter.com/CRFSuresnes">twitter</a> et <a href="http://www.facebook.com/pages/Croix-Rouge-francaise-delegation-de-Suresnes/125654477487031">facebook</a>)</p>
+<p><a href="http://validator.w3.org/check?uri=referer"><img src="images/html5.gif" alt="Site valide XHTML 1.1!" /></a></p>
+<!-- phpmyvisites -->
+<script type="text/javascript">
+<!--
+var a_vars = Array();
+var pagename='';
+var phpmyvisitesSite = 2;
+var phpmyvisitesURL = "http://vic.drago.free.fr/phpmv2/phpmyvisites.php";
+//-->
+</script>
+<script src="http://vic.drago.free.fr/phpmv2/phpmyvisites2.js" type="text/javascript"></script>
+<noscript>
+<p>
+<a href="http://www.phpmyvisites.net/" title="phpMyVisites | Open source web analytics">
+<img src="http://vic.drago.free.fr/phpmv2/phpmyvisites.php" alt="" style="border:0" />
+</a>
+</p>
+</noscript>
+<!-- /phpmyvisites -->
+</body>
+</html>
